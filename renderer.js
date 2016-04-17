d85223fb5697bf6d81b832ec7aec47d55daea5d8
ajuchem <ajuchem@daudt.com>
2016-04-17 12:36:57 -0600

    Replace placeholders with actual values

--- renderer.js
+++ renderer.js
@@ -1,11 +1,21 @@
 var fs = require('fs');
 
+function mergeValues(values, content) {
+  // cycle over the keys
+  for(var key in values) {
+    // replace all {{key}} w/ the value from the values object
+    content = content.replace("{{" + key + "}}", values[key]);
+  }
+  // return merged content
+  return content;
+}
+
 function view(templateName, values, res) {
   // read from the template files
-  var fileContents = fs.readFileSync('./views/' + templateName + '.html');
+  var fileContents = fs.readFileSync('./views/' + templateName + '.html', {encoding: "utf8"});
   // insert values into the content
-
-  // write out the contents to the response 
+  fileContents = mergeValues(values, fileContents)
+  // write out the contents to the response
   res.write(fileContents);
   }
 
